f53379ba333830f3e3d3a7dd35978acfcacfc272
feature_liwg <liwg@hyjx.com>
2020-09-07 17:57:04 +0800

    逆向工程生成

---
+++ src/main/java/com/lwg/vhr/mapper/RoleMapper.java
@@ -0,0 +1,30 @@
+package com.lwg.vhr.mapper;
+
+import com.lwg.vhr.pojo.Role;
+import com.lwg.vhr.pojo.RoleExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface RoleMapper {
+    long countByExample(RoleExample example);
+
+    int deleteByExample(RoleExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Role record);
+
+    int insertSelective(Role record);
+
+    List<Role> selectByExample(RoleExample example);
+
+    Role selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Role record, @Param("example") RoleExample example);
+
+    int updateByExample(@Param("record") Role record, @Param("example") RoleExample example);
+
+    int updateByPrimaryKeySelective(Role record);
+
+    int updateByPrimaryKey(Role record);
+}
