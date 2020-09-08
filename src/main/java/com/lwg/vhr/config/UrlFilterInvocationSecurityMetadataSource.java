e000d8209caba4a39079195355bbb4566ce36553
lwg729 <1098372951@qq.com>
2020-09-08 21:36:40 +0800

    配置角色对应的url进行拦截

--- src/main/java/com/lwg/vhr/config/UrlFilterInvocationSecurityMetadataSource.java
+++ src/main/java/com/lwg/vhr/config/UrlFilterInvocationSecurityMetadataSource.java
@@ -3,8 +3,10 @@ package com.lwg.vhr.config;
 import java.util.Collection;
 import java.util.List;
 
+import com.lwg.vhr.pojo.Role;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.access.SecurityConfig;
 import org.springframework.security.web.FilterInvocation;
 import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource;
 import org.springframework.stereotype.Component;
@@ -20,19 +22,32 @@ public class UrlFilterInvocationSecurityMetadataSource implements FilterInvocati
     private MenuServiceImpl menuService;
 
     AntPathMatcher antPathMatcher = new AntPathMatcher();
+
     @Override
     public Collection<ConfigAttribute> getAttributes(Object o) throws IllegalArgumentException {
 
         //获得请求的url
         String requestUrl = ((FilterInvocation) o).getRequestUrl();
 
+        //如果是登录页面,不需要角色的判别,返回null即可
+        if ("login_p".equals(requestUrl)) {
+            return null;
+        }
+
         List<Menu> allMenu = menuService.getAllMenuWithRole();
         for (Menu menu : allMenu) {
-            if (antPathMatcher.match(menu.getUrl(),requestUrl)&& menu.getRoles().size()>0){
+            if (antPathMatcher.match(menu.getUrl(), requestUrl) && menu.getRoles().size() > 0) {
+                List<Role> roles = menu.getRoles();
+                int size = roles.size();
+                String[] values = new String[size];
+                for (int i = 0; i < size; i++) {
+                    values[i] = roles.get(i).getName();
+                }
+                return SecurityConfig.createList(values);
 
             }
         }
-        return null;
+        return SecurityConfig.createList("ROLE_LOGIN");
     }
 
     @Override
